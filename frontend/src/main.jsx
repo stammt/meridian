ac234515f3d6b69e3e5f7339935864d76df9b87a
Tom Stamm <stammt@gmail.com>
2026-03-18 10:48:11 -0600

    Follow sentry recommendations for react

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,6 @@
+// Sentry initialization should be imported first!
+import "./instrument.js";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -74,29 +77,18 @@ function App() {
   );
 }
 
-Sentry.init({
-  dsn: import.meta.env.DEV
-    ? undefined
-    : "https://277fb224253f425636afd14600634a73@o4511066194640896.ingest.us.sentry.io/4511066195689472",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true,
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.,
-  // Enable logs to be sent to Sentry
-  enableLogs: true,
+const container = document.getElementById("root");
+const root = createRoot(container, {
+  // Callback called when an error is thrown and not caught by an ErrorBoundary.
+  onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
+    console.warn("Uncaught error", error, errorInfo.componentStack);
+  }),
+  // Callback called when React catches an error in an ErrorBoundary.
+  onCaughtError: Sentry.reactErrorHandler(),
+  // Callback called when React automatically recovers from errors.
+  onRecoverableError: Sentry.reactErrorHandler(),
 });
-
-createRoot(document.getElementById("root")).render(
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
